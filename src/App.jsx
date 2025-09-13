2c82d5bd8f90cf521f92a80499c2e46aa7cdd446
Charishma24003 <charishmais003@gmail.com>
2025-09-13 12:47:29 +0530

    Installed the react components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <nav className='nav'>
+        Navbar Content
+      </nav>
+      <main >
+        Main Content
+      </main>
+     
+    </div>
+  )
+}
+
+export default App
